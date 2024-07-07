41810f687e77b26f4c9464883aa8a596316c5893
devkrishna <ambatikrishnasai@gmail.com>
2024-07-08 04:32:08 +0530

    for deployment changes made

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,34 +49,34 @@ export default function Home() {
 
   const { isSignedIn } = useUser();
 
-  const handleClick = () => {
-    const duration = 5 * 1000;
-    const animationEnd = Date.now() + duration;
-    const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+  // const handleClick = () => {
+  //   const duration = 5 * 1000;
+  //   const animationEnd = Date.now() + duration;
+  //   const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
 
-    const randomInRange = (min: number, max: number) =>
-      Math.random() * (max - min) + min;
+  //   const randomInRange = (min: number, max: number) =>
+  //     Math.random() * (max - min) + min;
 
-    const interval = window.setInterval(() => {
-      const timeLeft = animationEnd - Date.now();
+  //   const interval = window.setInterval(() => {
+  //     const timeLeft = animationEnd - Date.now();
 
-      if (timeLeft <= 0) {
-        return clearInterval(interval);
-      }
+  //     if (timeLeft <= 0) {
+  //       return clearInterval(interval);
+  //     }
 
-      const particleCount = 50 * (timeLeft / duration);
-      confetti({
-        ...defaults,
-        particleCount,
-        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
-      });
-      confetti({
-        ...defaults,
-        particleCount,
-        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
-      });
-    }, 250);
-  };
+  //     const particleCount = 50 * (timeLeft / duration);
+  //     confetti({
+  //       ...defaults,
+  //       particleCount,
+  //       origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+  //     });
+  //     confetti({
+  //       ...defaults,
+  //       particleCount,
+  //       origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+  //     });
+  //   }, 250);
+  // };
 
   return (
     <div className="w-full min-h-screen bg-black text-white">
@@ -189,11 +189,11 @@ export default function Home() {
                     {isSignedIn ? (
                       <Link href="/courses">✨ Go to Courses</Link>
                     ) : (
-                      <Link href="#signin" onClick={handleClick}>
-                        <SignInButton forceRedirectUrl="/courses" mode="modal">
-                          ✨ Get Started
-                        </SignInButton>
-                      </Link>
+                      // <Link href="#signin" onClick={handleClick}>
+                      <SignInButton forceRedirectUrl="/courses" mode="modal">
+                        ✨ Get Started
+                      </SignInButton>
+                      // </Link>
                     )}
                     <ArrowRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
                   </AnimatedShinyText>
