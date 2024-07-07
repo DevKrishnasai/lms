56a210507c46c5bbe4c1832f71858c9fb2c8fcaa
Ambati Krishna Sai <122152880+DevKrishnasai@users.noreply.github.com>
2024-07-08 05:24:19 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -102,7 +102,7 @@ export default function Home() {
               ))}
             </div>
             {/* </div> */}
-            <div className="md:hidden">
+            <div className="hidden">
               <button
                 onClick={() => setIsMenuOpen(!isMenuOpen)}
                 className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
