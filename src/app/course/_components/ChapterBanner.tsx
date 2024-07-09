2e193f27dfe2aab3163687e9f8ec1f094232f114
Ambati Krishna Sai <122152880+DevKrishnasai@users.noreply.github.com>
2024-07-09 22:57:21 +0530

    added a class for banner

--- src/app/course/_components/ChapterBanner.tsx
+++ src/app/course/_components/ChapterBanner.tsx
@@ -16,7 +16,7 @@ const ChapterBanner = ({ isCompleted }: ChapterBannerProps) => {
   return (
     <div className="w-full">
       <div
-        className="bg-yellow-300 border border-red-400 text-red-700 px-4 py-3"
+        className="bg-yellow-300 border flex gap-3 border-red-400 text-red-700 px-4 py-3"
         role="banner"
       >
         <BookMarked className="w-5 h-5 inline-block mr-1" />
